b806bb18aa3fca34c498b6d8a1cdded5bf7eaafb
Kieran <kdleschinski@gmail.com>
2021-10-21 11:58:02 +0200

    adding tomas stolkers update

--- build/lib/skycalc_ipy/__init__.py
+++
@@ -1,8 +0,0 @@
-import warnings as _warnings
-from astropy.utils.exceptions import AstropyWarning as _AstropyWarning
-_warnings.simplefilter('ignore', category=_AstropyWarning)
-
-from . import ui
-from . import core
-
-from .ui import SkyCalc
